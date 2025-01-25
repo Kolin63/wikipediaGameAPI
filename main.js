ad8e1388fe8940746cc176f516d108305897b4d3
Colin Melican <theverylastgoodname@gmail.com>
2025-01-24 20:15:32 -0500

    Mainpage and /link return HTML

--- main.js
+++ main.js
@@ -7,14 +7,32 @@ const PORT = 3001;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.text());
 
 app.post("/links", async (req, res) => {
-    let link = "en.wikipedia.org/wiki/" + req.body;
-    console.log(link);
+    let link = "https://en.wikipedia.org/wiki/" + req.body;
+    console.log("POST " + link);
+
+    fetch(link)
+    .then(response => {
+        return response.text();
+    })
+    .then(html => {
+        console.log(html);
+    });
+
+    res.end(link);
 });
 
 app.get("/", async (req, res) => {
-    console.log("TEST");
+    console.log("GET /");
+    res.end(JSON.stringify({
+        "Welcome to": "Wikipedia Game API",
+        "By": "Kolin63",
+        "About": "Fetch all links that lead out of a Wikipedia Page!",
+        "Usage": "TODO",
+        "GitHub": "https://github.com/Kolin63/wikipediaGameApi"
+    }, null, 3));
 })
 
 app.listen(PORT, () => {
